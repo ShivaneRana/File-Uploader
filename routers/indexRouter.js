cefc6b849c5877f4817a5aa4ebf556c80b0fbbf8
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-02-28 22:52:45 +0530

    chore: root path added

---
+++ routers/indexRouter.js
@@ -0,0 +1,9 @@
+const {Router} = require('express');
+
+const indexRouter = Router();
+
+indexRouter.get('/',(req,res) => {
+    res.status(200).render("index");
+})
+
+module.exports = indexRouter;
