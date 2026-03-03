03ffdde733c2505acaddbcecad27bf1fab4a5b7c
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-03-03 22:58:57 +0530

    feat: conditional redirect based on login info

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -3,7 +3,15 @@ const { Router } = require("express");
 const indexRouter = Router();
 
 indexRouter.get("/", (req, res) => {
-	res.status(200).render("index");
+	if(!req.user){
+		return res.status(200).redirect("/login");
+	}else{
+		return res.status(200).redirect("/home");
+	}
 });
+
+indexRouter.get('/home',(req,res) => {
+	return res.status(200).render("index")
+})
 
 module.exports = indexRouter;
