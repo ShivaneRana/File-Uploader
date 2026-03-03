e57132d1bd92e9a9dcb44467e2326bfb550b907d
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-03-03 20:04:04 +0530

    feat: added controller for logging in users

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,3 +1,14 @@
+const { body,validationResult,matchedData } = require('express-validator')
+const db = require("../db/queries.js");
+const passport = require("passport");
+
 module.exports.showLoginPage = (req, res) => {
 	res.status(200).render("login");
 };
+
+module.exports.loginUser = async (req,res,next) => {
+	passport.authenticate("local",{
+		successRedirect:'/',
+		failureRedirect:'/login'
+	})(req,res,next);
+}
