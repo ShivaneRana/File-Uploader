8328d337d29a682f8e55b949fa857a9a831ce092
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-03-03 16:52:38 +0530

    chore: added passport JS local strategy.

--- app.js
+++ app.js
@@ -2,17 +2,50 @@ const express = require("express");
 const expressSession = require("express-session");
 const dotenv = require("dotenv").config({ quiet: true, debug: false });
 const path = require("node:path");
+const prisma = require("./lib/prisma.js")
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
+const {configurePassport} = require("./middlewares/passport.js")
+const passport = require("passport");
 
 // Routers
 const indexRouter = require("./routers/indexRouter.js");
 
 const SESSION_SECRET = process.env.SESSION_SECRET;
 
+const sessionStore = new PrismaSessionStore(prisma,{
+        checkPeriod: 24 * 60 * 60 * 1000,  //ms
+        dbRecordIdIsSessionId: true,
+        dbRecordIdFunction: undefined,
+})
+
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+
+// 
+app.use(
+  expressSession({
+    cookie: {
+     maxAge: 24 * 60 * 60 * 1000 // ms
+    },
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store:	sessionStore
+}));
+
+configurePassport(passport);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req,res,next) => {
+	const currentUser = req.user;
+	next();
+})
+
 
 app.get("/", indexRouter);
 
