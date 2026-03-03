7ca6bc4f933257adbf73d41f1d16eab324c24e56
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-03-03 19:17:04 +0530

    feat: implmented route for /login path and create router

---
+++ routers/registerRouter.js
@@ -0,0 +1,8 @@
+const registerController = require('../controllers/registerController.js');
+const { Router } = require("express")
+
+const registerRouter = Router();
+
+registerRouter.get('/',registerController.showRegisterPage);
+
+module.exports = registerRouter;
