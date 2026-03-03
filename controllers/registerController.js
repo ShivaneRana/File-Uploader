3052a2e976ad86d2466d3052415e0e5111f7b120
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-03-03 19:33:12 +0530

    feat: added controller for registerning usre

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,3 +1,15 @@
+const { matchedData,validationResult,body } = require('express-validator');
+const db = require('../db/queries.js');
+
 module.exports.showRegisterPage = (req, res) => {
 	res.status(200).render("register");
 };
+
+module.exports.registerUser = async (req,res) => {
+    const {username,password,email} = req.body;
+    console.log(username);
+    console.log(password);
+    console.log(email);
+    await db.createNewUser({username,password,email})
+    res.status(200).redirect("/");
+}
