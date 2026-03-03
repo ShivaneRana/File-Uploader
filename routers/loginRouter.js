03d196dc3a599a0621b0f7499e8f8a6021e8ffea
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-03-03 20:05:10 +0530

    feat: added controller for post method for root route

--- routers/loginRouter.js
+++ routers/loginRouter.js
@@ -4,5 +4,6 @@ const loginController = require("../controllers/loginController.js");
 const loginRouter = Router();
 
 loginRouter.get("/", loginController.showLoginPage);
+loginRouter.post("/", loginController.loginUser);
 
 module.exports = loginRouter;
