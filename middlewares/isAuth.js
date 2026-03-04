e5dbaf37fc480a9362bd102d77de7099421cc021
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-03-04 19:21:08 +0530

    chore: code formatted via prettier

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -1,7 +1,7 @@
-module.exports.isAuth = (req,res,next) => {
-	if(req.isAuthenticated()){
-        return next();
-    }
+module.exports.isAuth = (req, res, next) => {
+	if (req.isAuthenticated()) {
+		return next();
+	}
 
-    return res.status(401).redirect("/login")
-}
+	return res.status(401).redirect("/login");
+};
--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -1,18 +1,18 @@
 const { Router } = require("express");
-const {isAuth} = require("../middlewares/isAuth.js");
+const { isAuth } = require("../middlewares/isAuth.js");
 
 const indexRouter = Router();
 
 indexRouter.get("/", (req, res) => {
-	if(!req.user){
+	if (!req.user) {
 		return res.status(200).redirect("/login");
-	}else{
+	} else {
 		return res.status(200).redirect("/home");
 	}
 });
 
-indexRouter.get('/home',isAuth,(req,res) => {
-	return res.status(200).render("index")
-})
+indexRouter.get("/home", isAuth, (req, res) => {
+	return res.status(200).render("index");
+});
 
 module.exports = indexRouter;
