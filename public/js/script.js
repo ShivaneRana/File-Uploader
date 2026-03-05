bba81c76cbb431046acb1f13426554180d951a42
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-03-05 23:48:26 +0530

    chore: code formatted via prettier

--- public/js/script.js
+++ public/js/script.js
@@ -2,12 +2,12 @@ const dialogOption = document.querySelector(".dialog-option");
 const dialogContainer = document.querySelector(".index__dialog-container");
 const openButton = document.querySelector(".index__dialog-open-button");
 
-openButton.addEventListener("click",() => {
-    dialogOption.showModal();
-})
+openButton.addEventListener("click", () => {
+	dialogOption.showModal();
+});
 
-dialogOption.addEventListener("click",e => {
-  if(e.target !== dialogContainer){
-    dialogOption.close();
-  }
-})
+dialogOption.addEventListener("click", (e) => {
+	if (e.target !== dialogContainer) {
+		dialogOption.close();
+	}
+});
