5edf693b239a1267b42e6e11fd404270fae36f0f
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-03-06 17:19:11 +0530

    chore: code formatted via prettier

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,20 @@
 const openButton = document.querySelector(".index__open-file-upload-dialog");
 const closeButton = document.querySelector(".index__close-file-upload-dialog");
 const fileUploadDialog = document.querySelector(".index__file-upload-dialog");
-const fileUploadDialogContainer = document.querySelector(".index__file-upload-dialog-container");
-            
+const fileUploadDialogContainer = document.querySelector(
+	".index__file-upload-dialog-container",
+);
 
-
-openButton.addEventListener("click",() => {
+openButton.addEventListener("click", () => {
 	fileUploadDialog.showModal();
-})
+});
 
-fileUploadDialog.addEventListener('click',(e) => {
-	if(e.target === fileUploadDialog){
+fileUploadDialog.addEventListener("click", (e) => {
+	if (e.target === fileUploadDialog) {
 		fileUploadDialog.close();
 	}
-})
+});
 
-closeButton.addEventListener("click",() => {
+closeButton.addEventListener("click", () => {
 	fileUploadDialog.close();
-})
+});
