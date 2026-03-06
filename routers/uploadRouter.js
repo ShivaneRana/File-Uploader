20acf931391bafb1defe965bfb25958415668208
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-03-06 21:58:16 +0530

    chore: changed saveFile name

--- routers/uploadRouter.js
+++ routers/uploadRouter.js
@@ -8,11 +8,11 @@ const storage = multer.diskStorage({
 	},
 	filename: function (req, file, cb) {
 		const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-		cb(null, file.originalname + "-" + uniqueSuffix);
+		cb(null, uniqueSuffix + "-" +  file.originalname );
 	},
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({storage: storage});
 
 const uploadRouter = Router();
 
