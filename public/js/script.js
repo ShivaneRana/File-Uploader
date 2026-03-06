3904ccddc60c2fca09cd4a0b88c387f76a127a1e
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-03-06 17:17:01 +0530

    feat: dialog open and close logic added

---
+++ public/js/script.js
@@ -0,0 +1,20 @@
+const openButton = document.querySelector(".index__open-file-upload-dialog");
+const closeButton = document.querySelector(".index__close-file-upload-dialog");
+const fileUploadDialog = document.querySelector(".index__file-upload-dialog");
+const fileUploadDialogContainer = document.querySelector(".index__file-upload-dialog-container");
+            
+
+
+openButton.addEventListener("click",() => {
+	fileUploadDialog.showModal();
+})
+
+fileUploadDialog.addEventListener('click',(e) => {
+	if(e.target === fileUploadDialog){
+		fileUploadDialog.close();
+	}
+})
+
+closeButton.addEventListener("click",() => {
+	fileUploadDialog.close();
+})
