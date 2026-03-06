27bea74f5cf3fa6abc6febc90982448b51ef1270
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-03-06 20:35:52 +0530

    chore: minor changes to reroute all request to / to /home

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use((req, res, next) => {
 	next();
 });
 
-app.use("/home", indexRouter);
+app.use("/", indexRouter);
 app.use("/login", loginRouter);
 app.use("/register", registerRouter);
 
--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -3,7 +3,11 @@ const { isAuth } = require("../middlewares/isAuth.js");
 
 const indexRouter = Router();
 
-indexRouter.get("/", isAuth, (req, res) => {
+indexRouter.get('/', isAuth,(req,res) => {
+	return res.status(200).redirect("/home");
+})
+
+indexRouter.get("/home", isAuth, (req, res) => {
 	return res.status(200).render("index");
 });
 
