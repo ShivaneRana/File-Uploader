28a2f1e928cccac8c283c0ab919f149bfe0cada0
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-03-06 18:07:41 +0530

    chore: routes refactored

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -3,16 +3,13 @@ const { isAuth } = require("../middlewares/isAuth.js");
 
 const indexRouter = Router();
 
-indexRouter.get("/", (req, res) => {
-	if (!req.user) {
-		return res.status(200).redirect("/login");
-	} else {
-		return res.status(200).redirect("/home");
-	}
-});
-
-indexRouter.get("/home", isAuth, (req, res) => {
+indexRouter.get("/", isAuth, (req, res) => {
 	return res.status(200).render("index");
 });
+
+indexRouter.post("/upload",isAuth,(req,res) => {
+	console.log("upload route hit!!!!!!");
+	return res.status(200).redirect("/");
+})
 
 module.exports = indexRouter;
