52bf7ad2531c44c06c4b4026250951ec3c7ea427
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-03-06 21:43:21 +0530

    chore: code formatted via prettier

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -3,9 +3,9 @@ const { isAuth } = require("../middlewares/isAuth.js");
 
 const indexRouter = Router();
 
-indexRouter.get('/', isAuth,(req,res) => {
+indexRouter.get("/", isAuth, (req, res) => {
 	return res.status(200).redirect("/home");
-})
+});
 
 indexRouter.get("/home", isAuth, (req, res) => {
 	return res.status(200).render("index");
--- routers/uploadRouter.js
+++ routers/uploadRouter.js
@@ -3,23 +3,28 @@ const { isAuth } = require("../middlewares/isAuth.js");
 const multer = require("multer");
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads')
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.originalname + '-' + uniqueSuffix)
-  }
-})
+	destination: function (req, file, cb) {
+		cb(null, "./uploads");
+	},
+	filename: function (req, file, cb) {
+		const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+		cb(null, file.originalname + "-" + uniqueSuffix);
+	},
+});
 
-const upload = multer({storage:storage})
+const upload = multer({ storage: storage });
 
 const uploadRouter = Router();
 
-uploadRouter.post('/', isAuth, upload.single('input-file'), function (req,res) {
-	console.log("req file");
-	console.log(req.file);
-	return res.status(200).redirect("home");
-})
+uploadRouter.post(
+	"/",
+	isAuth,
+	upload.single("input-file"),
+	function (req, res) {
+		console.log("req file");
+		console.log(req.file);
+		return res.status(200).redirect("home");
+	},
+);
 
 module.exports = uploadRouter;
