354ff79841af1b7eaca67f6c20234aadc455f6ad
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-03-09 14:20:37 +0530

    feat: added logic for fetching folders and displaying them

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const { isAuth } = require("../middlewares/isAuth.js");
+const db = require("../db/queries.js");
 
 const indexRouter = Router();
 
@@ -7,8 +8,12 @@ indexRouter.get("/", isAuth, (req, res) => {
 	return res.status(200).redirect("/home");
 });
 
-indexRouter.get("/home", isAuth, (req, res) => {
-	return res.status(200).render("index");
+indexRouter.get("/home", isAuth, async(req, res) => {
+	const userId = Number(req.user.id)
+	const folderList = await db.fetchAllFolders({id: userId});
+	return res.status(200).render("index",{
+		folderList
+	});
 });
 
 module.exports = indexRouter;
