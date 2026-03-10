235012061a595edf2b09255c9e77683e40228a02
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-03-10 16:56:55 +0530

    feat: 1. added route for displaying file content of individual folders 2. implmeneted a query for deleting files first before deleting folders.

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -1,6 +1,8 @@
 const { Router } = require("express");
 const { isAuth } = require("../middlewares/isAuth.js");
 const db = require("../db/queries.js");
+const getMonth = require('../public/js/getMonths.js');
+const getSize = require('../public/js/getSize.js');
 
 const indexRouter = Router();
 
@@ -16,9 +18,38 @@ indexRouter.get("/home", isAuth, async (req, res) => {
 	});
 });
 
+indexRouter.get("/home/:folderId", isAuth, async (req, res) => {
+	let {folderId} = req.params;
+	folderId = Number(folderId);
+	let files = await db.fetchFilesByFolderId({folderId});
+
+	// this is for formatting data for better readability
+	files = files.map(file => {
+		return {
+			...file,
+			size_in_bytes: getSize(file.size_in_bytes),
+			createdAt: `${getMonth(file.createdAt.getMonth())} ${file.createdAt.getDate()}, ${file.createdAt.getFullYear()}`
+		};
+	})
+
+	console.log(files)
+
+	const userId = Number(req.user.id);
+	const folderList = await db.fetchAllFolders({ id: userId });
+
+	return res.status(200).render("index", {
+		folderList,
+		currentFolderId: folderId,
+		filesList: files
+	});
+});
+
+
 indexRouter.get("/home/folder/delete/:folderId", isAuth, async (req, res) => {
 	let { folderId } = req.params;
 	folderId = Number(folderId);
+	// delete files first inside folder then folder itself
+	await db.deleteFilesByFolderId({folderId: folderId});
 	await db.deleteFolder({ id: folderId });
 	return res.status(200).redirect("/home");
 });
