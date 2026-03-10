65bf12bde4db90b5be99fc4913d9098509fb7463
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-03-10 15:55:36 +0530

    feat: module to find month name via number created

---
+++ public/js/getMonths.js
@@ -0,0 +1,20 @@
+const months = [
+    'January',
+    'Febuary',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+
+function getMonth(num){
+    return months[num];
+}
+
+module.exports = getMonth;
