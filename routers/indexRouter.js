1763fe64498000ec2e53cc720079a2892501f50c
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-03-15 23:50:17 +0530

    refactor: moved formatting of data recieved via database to seprate files.

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -1,8 +1,6 @@
 const { Router } = require("express");
 const { isAuth } = require("../middlewares/isAuth.js");
 const db = require("../db/queries.js");
-const getMonth = require("../public/js/getMonths.js");
-const getSize = require("../public/js/getSize.js");
 
 const indexRouter = Router();
 
@@ -18,22 +16,6 @@ indexRouter.get("/home", isAuth, async (req, res) => {
 	let files = await db.fetchFilesByFolderId({ folderId: undefined});
 	let childFolders = await db.fetchAllFolderByParentId({ userId,parentId: undefined})
 
-	childFolders = childFolders.map((folder) => {
-		return{
-			...folder,
-			createdAt: `${getMonth(folder.createdAt.getMonth())} ${folder.createdAt.getDate()}, ${folder.createdAt.getFullYear()}`,
-		}
-	})
-
-	// this is for formatting data for better readability
-	files = files.map((file) => {
-		return {
-			...file,
-			size_in_bytes: getSize(file.size_in_bytes),
-			createdAt: `${getMonth(file.createdAt.getMonth())} ${file.createdAt.getDate()}, ${file.createdAt.getFullYear()}`,
-		};
-	});
-
 	return res.status(200).render("index", {
 		currentFolderId: undefined,
 		folderList,
@@ -45,19 +27,9 @@ indexRouter.get("/home", isAuth, async (req, res) => {
 indexRouter.get("/home/:folderId", isAuth, async (req, res) => {
 	let { folderId } = req.params;
 	folderId = Number(folderId);
-	let files = await db.fetchFilesByFolderId({ folderId });
-
-	// this is for formatting data for better readability
-	files = files.map((file) => {
-		return {
-			...file,
-			size_in_bytes: getSize(file.size_in_bytes),
-			createdAt: `${getMonth(file.createdAt.getMonth())} ${file.createdAt.getDate()}, ${file.createdAt.getFullYear()}`,
-		};
-	});
-
 	const userId = Number(req.user.id);
 	const folderList = await db.fetchAllFoldersByUserId({ id: userId });
+	let files = await db.fetchFilesByFolderId({ folderId });
 
 	return res.status(200).render("index", {
 		folderList,
@@ -69,6 +41,7 @@ indexRouter.get("/home/:folderId", isAuth, async (req, res) => {
 indexRouter.get("/home/folder/delete/:folderId", isAuth, async (req, res) => {
 	let { folderId } = req.params;
 	folderId = Number(folderId);
+
 	// delete files first inside folder then folder itself
 	await db.deleteFilesByFolderId({ folderId: folderId });
 	await db.deleteFolder({ id: folderId });
