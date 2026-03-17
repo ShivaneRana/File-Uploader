c8edffe86cb99f3b074901975206f4a6521ae42c
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-03-17 10:39:53 +0530

    feat: moved all route logic to indexController.

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -1,59 +1,12 @@
 const { Router } = require("express");
 const { isAuth } = require("../middlewares/isAuth.js");
-const db = require("../db/queries.js");
+const indexController = require("../controllers/indexController.js")
 
 const indexRouter = Router();
 
-indexRouter.get("/", isAuth, (req, res) => {
-	return res.status(200).redirect("/home");
-});
-
-indexRouter.get("/home", isAuth, async (req, res) => {
-	const userId = Number(req.user.id);
-	const folderList = await db.fetchAllFoldersByUserId({ id: userId });
-
-	// files and folder present inside home directory have null folderId and parentId
-	let files = await db.fetchFilesByFolderId({ folderId: undefined });
-	let childFolders = await db.fetchAllFolderByParentId({
-		userId,
-		parentId: undefined,
-	});
-
-	return res.status(200).render("index", {
-		currentFolderId: undefined,
-		folderList,
-		filesList: files,
-		childFolders,
-	});
-});
-
-indexRouter.get("/home/:folderId", isAuth, async (req, res) => {
-	let { folderId } = req.params;
-	folderId = Number(folderId);
-	const userId = Number(req.user.id);
-	const folderList = await db.fetchAllFoldersByUserId({ id: userId });
-	let files = await db.fetchFilesByFolderId({ folderId });
-	let childFolders = await db.fetchAllFolderByParentId({
-		userId,
-		parentId: folderId,
-	});
-
-	return res.status(200).render("index", {
-		folderList,
-		currentFolderId: folderId,
-		filesList: files,
-		childFolders,
-	});
-});
-
-indexRouter.get("/home/folder/delete/:folderId", isAuth, async (req, res) => {
-	let { folderId } = req.params;
-	folderId = Number(folderId);
-
-	// delete files first inside folder then folder itself
-	await db.deleteFilesByFolderId({ folderId: folderId });
-	await db.deleteFolder({ id: folderId });
-	return res.status(200).redirect("/home");
-});
+indexRouter.get("/", isAuth, indexController.redirectToHomePage);
+indexRouter.get("/home", isAuth, indexController.renderHomePage);
+indexRouter.get("/home/:folderId", isAuth, indexController.renderSpecificPage);
+indexRouter.get("/home/folder/delete/:folderId", isAuth, indexController.deleteFolder);
 
 module.exports = indexRouter;
