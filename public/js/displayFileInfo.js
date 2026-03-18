8e2a6b0d49aa4117c72a62b3981df3a58d6a3548
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-03-18 20:44:09 +0530

    feat: added logic for displaying file information.

--- public/js/displayFileInfo.js
+++ public/js/displayFileInfo.js
@@ -1,5 +1,65 @@
 const uploadedFiles = document.querySelectorAll(".uploaded-files");
 const fileInfoDiv = document.querySelector(".file-info-div");
+const fileInfo = {
+	name: undefined,
+	type: undefined,
+	size: undefined,
+	date: undefined
+}
+const fileName = document.createElement("p");
+const fileSize = document.createElement("p");
+const fileDate = document.createElement("p");
+const fileType = document.createElement("p");
+
+
+// file info Div heading 
+const fileInfoHeader = document.createElement("div");
+const fileInfoCloseButton = document.createElement("button");
+const fileInfoHeading = document.createElement("h3");
+
+
+// this is where file name ,type ,size and date will be kept
+const fileInfoContent = document.createElement("div");
+
+
+// file info Div footer
+const fileInfoFooter = document.createElement("div");
+const fileInfoDownloadLink = document.createElement("a");
+const fileInfoShareLink = document.createElement("button");
+const fileInfoDeleteLink = document.createElement("a");
+const fileInfoCloseDivFooter = document.createElement("button");
+
+
+// assing class to fileInfoContent where file name,type,size and date is stored.
+fileInfoContent.classList.add("file-info-content");
+
+
+// assign content to header element and append to parent div
+fileInfoCloseButton.textContent = 'X';
+fileInfoHeading.textContent = `File Information`;
+fileInfoHeader.append(fileInfoHeading,fileInfoCloseButton);
+fileInfoHeader.classList.add("file-info-header");
+
+
+// assign content to footer element and append to parent div
+fileInfoCloseDivFooter.textContent = "Close";
+fileInfoDeleteLink.textContent = "Delete";
+fileInfoShareLink.textContent = "Share";
+fileInfoDownloadLink.textContent = "Download";
+fileInfoFooter.append(fileInfoCloseDivFooter,fileInfoShareLink,fileInfoDeleteLink,fileInfoDownloadLink);
+fileInfoFooter.classList.add("file-info-footer");
+
+fileInfoCloseButton.addEventListener("click", (e) => {
+	e.stopPropagation();
+	fileInfoDiv.classList.remove("show-info");
+	fileInfoDiv.classList.add("hide-info");
+});
+
+fileInfoCloseDivFooter.addEventListener("click",(e) => {
+	e.stopPropagation();
+	fileInfoDiv.classList.remove("show-info");
+	fileInfoDiv.classList.add("hide-info");
+})
 
 document.body.addEventListener("click", () => {
 	if (fileInfoDiv.classList.contains("show-info")) {
@@ -15,32 +75,19 @@ fileInfoDiv.addEventListener("click", (e) => {
 uploadedFiles.forEach((file) => {
 	file.addEventListener("click", (e) => {
 		e.stopPropagation();
-		const heading = document.createElement("h3");
-		heading.textContent = "File Information";
 
-		const name = document.createElement("p");
-		name.textContent = file.dataset.name;
+		fileInfo.name = file.dataset.name;
+		fileInfo.size = file.dataset.size;
+		fileInfo.date = file.dataset.date;
+		fileInfo.type = file.dataset.type;
 
-		const size = document.createElement("p");
-		size.textContent = file.dataset.size;
+		fileName.textContent = `Name: ${fileInfo.name}`;
+		fileType.textContent = `Type: ${fileInfo.type}`;
+		fileSize.textContent = `Size: ${fileInfo.size}`;
+		fileDate.textContent = `Date: ${fileInfo.date}`;
 
-		const date = document.createElement("p");
-		date.textContent = file.dataset.date;
-
-		const type = document.createElement("p");
-		type.textContent = file.dataset.type;
-
-		const button = document.createElement("button");
-		button.textContent = "close";
-
-		button.addEventListener("click", (e) => {
-			e.stopPropagation();
-			fileInfoDiv.classList.remove("show-info");
-			fileInfoDiv.classList.add("hide-info");
-		});
-
-		fileInfoDiv.textContent = "";
-		fileInfoDiv.append(heading, name, size, date, type, button);
+		fileInfoContent.append(fileName,fileType,fileSize,fileDate);
+		fileInfoDiv.append(fileInfoHeader,fileInfoContent,fileInfoFooter);
 		fileInfoDiv.classList.remove("hide-info");
 		fileInfoDiv.classList.add("show-info");
 	});
