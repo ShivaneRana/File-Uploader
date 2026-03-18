455762f46094a78f1ec42fd68ff7fa2dc0eb0921
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-03-18 20:44:44 +0530

    chore: code formatted via prettier

--- public/js/displayFileInfo.js
+++ public/js/displayFileInfo.js
@@ -4,23 +4,20 @@ const fileInfo = {
 	name: undefined,
 	type: undefined,
 	size: undefined,
-	date: undefined
-}
+	date: undefined,
+};
 const fileName = document.createElement("p");
 const fileSize = document.createElement("p");
 const fileDate = document.createElement("p");
 const fileType = document.createElement("p");
 
-
-// file info Div heading 
+// file info Div heading
 const fileInfoHeader = document.createElement("div");
 const fileInfoCloseButton = document.createElement("button");
 const fileInfoHeading = document.createElement("h3");
 
-
 // this is where file name ,type ,size and date will be kept
 const fileInfoContent = document.createElement("div");
-
 
 // file info Div footer
 const fileInfoFooter = document.createElement("div");
@@ -29,24 +26,26 @@ const fileInfoShareLink = document.createElement("button");
 const fileInfoDeleteLink = document.createElement("a");
 const fileInfoCloseDivFooter = document.createElement("button");
 
-
 // assing class to fileInfoContent where file name,type,size and date is stored.
 fileInfoContent.classList.add("file-info-content");
 
-
 // assign content to header element and append to parent div
-fileInfoCloseButton.textContent = 'X';
+fileInfoCloseButton.textContent = "X";
 fileInfoHeading.textContent = `File Information`;
-fileInfoHeader.append(fileInfoHeading,fileInfoCloseButton);
+fileInfoHeader.append(fileInfoHeading, fileInfoCloseButton);
 fileInfoHeader.classList.add("file-info-header");
-
 
 // assign content to footer element and append to parent div
 fileInfoCloseDivFooter.textContent = "Close";
 fileInfoDeleteLink.textContent = "Delete";
 fileInfoShareLink.textContent = "Share";
 fileInfoDownloadLink.textContent = "Download";
-fileInfoFooter.append(fileInfoCloseDivFooter,fileInfoShareLink,fileInfoDeleteLink,fileInfoDownloadLink);
+fileInfoFooter.append(
+	fileInfoCloseDivFooter,
+	fileInfoShareLink,
+	fileInfoDeleteLink,
+	fileInfoDownloadLink,
+);
 fileInfoFooter.classList.add("file-info-footer");
 
 fileInfoCloseButton.addEventListener("click", (e) => {
@@ -55,11 +54,11 @@ fileInfoCloseButton.addEventListener("click", (e) => {
 	fileInfoDiv.classList.add("hide-info");
 });
 
-fileInfoCloseDivFooter.addEventListener("click",(e) => {
+fileInfoCloseDivFooter.addEventListener("click", (e) => {
 	e.stopPropagation();
 	fileInfoDiv.classList.remove("show-info");
 	fileInfoDiv.classList.add("hide-info");
-})
+});
 
 document.body.addEventListener("click", () => {
 	if (fileInfoDiv.classList.contains("show-info")) {
@@ -86,8 +85,8 @@ uploadedFiles.forEach((file) => {
 		fileSize.textContent = `Size: ${fileInfo.size}`;
 		fileDate.textContent = `Date: ${fileInfo.date}`;
 
-		fileInfoContent.append(fileName,fileType,fileSize,fileDate);
-		fileInfoDiv.append(fileInfoHeader,fileInfoContent,fileInfoFooter);
+		fileInfoContent.append(fileName, fileType, fileSize, fileDate);
+		fileInfoDiv.append(fileInfoHeader, fileInfoContent, fileInfoFooter);
 		fileInfoDiv.classList.remove("hide-info");
 		fileInfoDiv.classList.add("show-info");
 	});
