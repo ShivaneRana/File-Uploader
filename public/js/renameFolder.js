ac486fd20e9661b8895260e265abe99091cac8bc
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-03-25 17:23:22 +0530

    feat: added logic for displaying rename folder dialog box

---
+++ public/js/renameFolder.js
@@ -0,0 +1,20 @@
+// rename folder dialog
+const openRenameFolderButton = document.querySelector(".open-rename-folder-dialog");
+const renameFolderDialog = document.querySelector(".dialog-rename-folder");
+const renameFolderInput = document.querySelector("#rename-folder-input");
+const closeRenameFolderButton = document.querySelector(".close-folder-rename-dialog");
+
+openRenameFolderButton.addEventListener("click",(e) => {
+	renameFolderDialog.showModal();
+	renameFolderInput.focus();
+})
+
+renameFolderDialog.addEventListener("click", (e) => {
+	if (e.target === renameFolderDialog) {
+		renameFolderDialog.close();
+	}
+});
+
+closeRenameFolderButton.addEventListener("click",(e) => {
+	renameFolderDialog.close();
+});
