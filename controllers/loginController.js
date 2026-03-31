139a34dba09e2c82cd28ace99915a94fc0408274
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-03-31 20:37:03 +0530

    feat: added for validation for input fields

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,13 +1,47 @@
 const { body, validationResult, matchedData } = require("express-validator");
+const db = require("../db/queries.js");
 const passport = require("passport");
+
+const maxCharacters = 64
+const minCharacters= 3
+let username = '';
+let password = '';
+
+const validationObject = [
+	body("username")
+		.trim()
+		.notEmpty()
+		.withMessage("Username cannot be empty").bail()
+		.isLength({ min: minCharacters,max: maxCharacters })
+		.withMessage(`Username should be between ${minCharacters} and ${maxCharacters} characters`).bail()
+		.matches(/^[A-Za-z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/)
+		.withMessage('Username can contain only letters, numbers, and special characters').bail(),
+
+	body('password')
+		.trim()
+		.notEmpty()
+		.withMessage('Password cannot be empty').bail()
+		.isLength({ min: minCharacters, max: maxCharacters })
+		.withMessage(`Password should be between ${minCharacters} and ${maxCharacters} characters`).bail()
+		.matches(/^[A-Za-z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/)
+		.withMessage('Password can contain only letters, numbers, and special characters').bail()
+]
 
 module.exports.showLoginPage = (req, res) => {
 	res.status(200).render("login");
 };
 
-module.exports.loginUser = async (req, res, next) => {
-	passport.authenticate("local", {
-		successRedirect: "/home",
-		failureRedirect: "/login",
-	})(req, res, next);
-};
+module.exports.loginUser = [
+	validationObject,
+	async(req,res,next) => {
+		const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(200).render('login', { errors: errors.array() });
+        }
+
+		passport.authenticate("local", {
+			successRedirect: "/home",
+			failureRedirect: "/login",
+		})(req, res, next);
+	}
+]
