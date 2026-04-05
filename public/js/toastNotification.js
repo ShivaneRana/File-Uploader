eb76af2fc2085ce934c3fae0d2207ef2daf6d003
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-04-06 00:03:08 +0530

    feat: added logic for toast notification.

---
+++ public/js/toastNotification.js
@@ -0,0 +1,26 @@
+const toastNotification = document.querySelector(".toast-notification");
+const toastMessage = document.querySelector(".toast-message");
+let timerCount = null;
+const countdown = 1000;
+
+document.addEventListener("DOMContentLoaded",(e) => {
+  showToast();
+  timerCount = setTimeout(() => {
+    hideToast();
+  },countdown);
+});
+
+toastNotification.addEventListener("click",(e) => {
+  hideToast();
+})
+
+function showToast(){
+  toastNotification.classList.remove("hide-toast");
+  toastNotification.classList.add("show-toast")
+}
+
+function hideToast(){
+  clearTimeout(timerCount);
+  toastNotification.classList.remove("show-toast");
+  toastNotification.classList.add("hide-toast");
+}
