fd65bf122c179c3aed495fcc93bf5a6b9248f1a1
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-04-13 10:32:54 +0530

    chore: code formatted via prettier

--- db/queries.js
+++ db/queries.js
@@ -263,13 +263,13 @@ module.exports.fetchFileByFileId = async ({ fileId, userId }) => {
 };
 
 module.exports.checkIfUsernameExists = async ({ username }) => {
-    try {
-        const result = await prisma.user.findFirst({
-            where: { username: username },
-        });
-        return result ? true : false;
-    } catch (err) {
-        console.error("DB error in checkIfUsernameExists:", err);
-        throw new Error("Database error while checking username");
-    }
+	try {
+		const result = await prisma.user.findFirst({
+			where: { username: username },
+		});
+		return result ? true : false;
+	} catch (err) {
+		console.error("DB error in checkIfUsernameExists:", err);
+		throw new Error("Database error while checking username");
+	}
 };
